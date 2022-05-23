a2fab2d03e340b2280576471a616d2bf1748947f
abbas jradi <jradiabbass@gmail.com>
2022-05-23 11:14:22 -0700

    Generating dice image based on what number has been generated

--- javvascript.js
+++ javvascript.js
@@ -7,3 +7,64 @@ function randomIntFromInterval(min, max) {
 
 player1 = randomIntFromInterval(1, 6);
 player2 = randomIntFromInterval(1, 6);
+
+if( player1 === 1)
+{
+    document.getElementsByClassName("img1")[0].src="images/dice1.png";
+}
+else if(player1 === 2)
+{
+    document.getElementsByClassName("img1")[0].src="images/dice2.png";
+
+}
+else if(player1 === 3)
+{
+    document.getElementsByClassName("img1")[0].src="images/dice3.png";
+
+}
+else if(player1 === 4)
+{
+    document.getElementsByClassName("img1")[0].src="images/dice4.png";
+
+}
+else if(player1 === 5)
+{
+    document.getElementsByClassName("img1")[0].src="images/dice5.png";
+
+}
+else if(player1 === 6)
+{
+    document.getElementsByClassName("img1")[0].src="images/dice6.png";
+
+}
+
+
+if( player2 === 1)
+{
+    document.getElementsByClassName("img2")[0].src="images/dice1.png";
+}
+else if(player2 === 2)
+{
+    document.getElementsByClassName("img2")[0].src="images/dice2.png";
+
+}
+else if(player2 === 3)
+{
+    document.getElementsByClassName("img2")[0].src="images/dice3.png";
+
+}
+else if(player2 === 4)
+{
+    document.getElementsByClassName("img2")[0].src="images/dice4.png";
+
+}
+else if(player2=== 5)
+{
+    document.getElementsByClassName("img2")[0].src="images/dice5.png";
+
+}
+else if(player2 === 6)
+{
+    document.getElementsByClassName("img2")[0].src="images/dice6.png";
+
+}
