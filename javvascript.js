638833c6652b1783b9754dd1b546d31a9023731d
abbas jradi <jradiabbass@gmail.com>
2022-05-23 11:13:03 -0700

    Restricting the number between 1 and 6

--- javvascript.js
+++ javvascript.js
@@ -3,4 +3,7 @@ var player2 = 0;
 
 function randomIntFromInterval(min, max) { 
     return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+}
+
+player1 = randomIntFromInterval(1, 6);
+player2 = randomIntFromInterval(1, 6);
